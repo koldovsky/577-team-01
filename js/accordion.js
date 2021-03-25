f58ec9a0adef32be0be85ca20a801d07b456022e
mac <mac@macs-MacBook-Pro.local>
2021-03-25 23:33:31 +0200

    accordion add

--- js/accordion.js
+++ js/accordion.js
@@ -1,7 +1,35 @@
 "use strict";
 
-const acc = document.getElementsByClassName("accordion");
+let acc = document.getElementsByClassName("accordion");
 
-for(let i = 0; i<acc.length; i++){
-    console.log(acc[i]);
+
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+
+        this.classList.toggle("active");
+
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block" && panel.style.maxHeight) {
+            panel.style.display = "none";
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.display = "block";
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
 }
+
+
+// for (let i = 0; i < acc.length; i++) {
+//     acc[i].addEventListener("click", function () {
+
+//         this.classList.toggle("active");
+
+//         let panel = this.nextElementSibling;
+//         if (panel.style.maxHeight) {
+//             panel.style.maxHeight = null;
+//         } else {
+//             panel.style.maxHeight = panel.scrollHeight + "px";
+//         }
+//     });
+// }

0621710775ce470aa6b486972ca36f657657afc0
mac <mac@macs-MacBook-Pro.local>
2021-03-28 23:46:42 +0300

    upgrade accordion

--- js/accordion.js
+++ js/accordion.js
@@ -2,19 +2,24 @@
 
 const acc = document.getElementsByClassName("accordion");
 
-
 for (let i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-
-        this.classList.toggle("active");
-
-        const panel = this.nextElementSibling;
-        if (panel.style.display === "block" && panel.style.maxHeight) {
-            panel.style.display = "none";
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.display = "block";
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
+  acc[i].addEventListener("click", function () {
+    hideAll();
+    this.classList.toggle("active");
+    this.nextElementSibling.classList.toggle("active");
+    if (this.nextElementSibling.style.maxHeight) {
+      this.nextElementSibling.style.maxHeight = null;
+    } else {
+      this.nextElementSibling.style.maxHeight =
+        this.nextElementSibling.scrollHeight + "px";
+    }
+  });
 }
+function hideAll() {
+  for (let i = 0; i < acc.length; i++) {
+    acc[i].classList.toggle("active", false);
+    acc[i].nextElementSibling.classList.toggle("active", false);
+    acc[i].nextElementSibling.style.maxHeight = null;
+}
+}
+

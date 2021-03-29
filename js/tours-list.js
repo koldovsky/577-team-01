64abe2877b3c178f1b0cdb7681e02de6e5aa1d8a
Halyna-B <galya.berdik@gmail.com>
2021-03-29 16:05:17 +0300

    added bootstrap modal window

--- js/tours-list.js
+++ js/tours-list.js
@@ -25,40 +25,63 @@ class ToursList {
           }
         }
         [...tours].sort((a,b)=>this.sortDirection === 'ascending' ? a.price - b.price : b.price - a.price).forEach(tour=>{
-            toursListDomString += 
-    ` <article>
-            <img src="img/${tour.imageURL}" alt="${tour.title}">
-            <h2>${tour.title}</h2>
-            <p>${tour.description}</p>
-               <div class="button-container">
-                     <button class="info-btn"><span>Info</span></button>
-                     <button class="info-btn"><span>${(tour.price*rate).toFixed(2)} ${changeCurrIcon()} </span></button>
-               </div>
-       </article>`
-        }
-        );
-        this.container.innerHTML = toursListDomString;
-    }
-    async addEventListeners() {
-        document.querySelector('#show-option').addEventListener('click', async()=>{
-            this.option.value;
-            await this.renderTours();
-            this.addEventListeners();
-        }
-        );
+          toursListDomString +=  ` <article>
+          <img class="card-img-top" src="img/${tour.imageURL}" alt="${tour.title}">
+          <h2 class="card-title">${tour.title}</h2>
+          <p class="card-text">${tour.description}</p>
+             <div class="button-container">
+                   <button class="info-btn  btn btn-info "  data-bs-toggle="modal" data-bs-target="#productInfoModal" data-id='${tour.id}'><span class=" btn btn-info" data-bs-toggle="modal" data-bs-target="#productInfoModal" data-id='${tour.id}'>Info</span></button>
+                   <button class="info-btn buy"><span>${(tour.price*rate).toFixed(2)} ${changeCurrIcon()} </span></button>
+             </div>
+     </article>`
+  });
+  this.container.innerHTML = toursListDomString;
+}
+   async addEventListeners() {
 
-        document.querySelector('.sort-asc').addEventListener('click', async()=>{
-            this.sortDirection = 'ascending';
-            await this.renderTours();
-            this.addEventListeners();
-        }
-        );
-        document.querySelector('.sort-dsc').addEventListener('click', async()=>{
-            this.sortDirection = 'descending';
-            await this.renderTours();
-            this.addEventListeners();
-        }
-        );
+    document
+    .querySelectorAll('.button-container .btn-info')
+    .forEach(button =>
+      button.addEventListener('click', event =>
+        this.handleProductInfoClick(event)
+      )
+    );
 
-    }
+
+      document
+        .querySelector('#show-option').addEventListener('click', async () => {
+        this.option.value ;
+        await this.renderTours();
+        this.addEventListeners();
+      });
+
+      document
+      .querySelector('.sort-asc').addEventListener('click', async () => {
+      this.sortDirection = 'ascending';
+      await this.renderTours();
+      this.addEventListeners();
+  });
+      document
+      .querySelector('.sort-dsc').addEventListener('click', async () => {
+      this.sortDirection = 'descending';
+      await this.renderTours();
+      this.addEventListeners();
+  });
+
+}
+async handleProductInfoClick(event) {
+  const button = event.target; // Button that triggered the modal
+  const id = button.dataset.id; // Extract info from data-* attributes
+  const tour = await this.toursService.getToursById(id);
+  const modal = document.querySelector('#productInfoModal');
+  const tourImg = modal.querySelector('.card-img-top');
+  tourImg.setAttribute('src','img/' + tour.imageURL);
+  tourImg.setAttribute('alt', tour.title);
+  modal.querySelector('.modal-body .card-title').innerText = tour.title;
+  modal.querySelector('.modal-body .card-text').innerText =
+    tour.description;
+  const btnBuy = modal.querySelector('.buy');
+  btnBuy.innerText = tour.price + " NOK";
+  btnBuy.dataset.id = id;
+}
 }
